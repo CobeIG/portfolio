2962d2cf9e7d5ad1d783d63251dafb006e33c28c
Jacobe Foster <Jacobe.Foster@sckans.edu>
2021-09-21 10:54:24 -0500

    edit

---
+++ src/views/EasterEgg.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+const EasterEgg = () => {
+    return (
+        <div class= "inner-inner-MLB">
+            <div class ="nav">
+                <Link to="/portfolio">
+                    HomePage.com
+                </Link>
+            </div>
+            I play PS5 MLB the show
+            <div class="inner-MLB">
+                I hit Straight BOMBSSSSS
+                <div class="inner-inner-Game">
+                    I also play Brawlhalla
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default EasterEgg;
